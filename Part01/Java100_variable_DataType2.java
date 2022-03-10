d8db2cc57f14df789aebe550452d82d249d49e32
Yulia <oing922@gmail.com>
2022-03-11 08:37:41 +0900

    변수 초기화

--- Part01/Java100_variable_DataType2.java
+++ Part01/Java100_variable_DataType2.java
@@ -1,5 +1,21 @@
-public class Java100_variable_DateType2{
-	public static void main(String[] args) {
+public class Java100_variable_DataType2{
+	public static void main(String[] args){
+		int w;
+		int x, y, z;
+		x = 900;
+		y = 900;
+		z = 900;
+		System.out.println(x + ", " + y + ", " + z);
 		
+		int x1=700, y1=800, z1=900;
+		System.out.println(x1 + ", " + y1 + ", " + z1);
+		
+		String str1, str2, str3;
+		str1 = str2 = str3 = "KR";
+		System.out.println(str1 + ", " + str2 + ", " + str3);
+		
+		int i, j, k;
+		i = j = k = 100;
+		System.out.println(i + ", " + j + ", " + k);
 	}
 }
