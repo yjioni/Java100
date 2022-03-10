d0947f182d90838d432571ee31ac892e8ee90abe
Yulia <oing922@gmail.com>
2022-03-11 08:11:52 +0900

    Err 찾기

---
+++ Part01/Java100_variable_DataType1.java
@@ -0,0 +1,21 @@
+public class Java100_variable_DataType1 {
+	public static void main(String[] args) {
+
+		int a;
+		int b; int c = 90;
+		double d;
+		char e;
+		
+		a = 10;
+		b = 2;  //-- 변수 초기화 필수
+		d = 10;
+		e = 'A'; //-- "A" 로 하면 err
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+
+	}
+}
