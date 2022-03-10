be3aa1a7e8491e6bf7af2858b015a5bcdbf1e903
Yulia <oing922@gmail.com>
2022-03-11 08:31:32 +0900

    BYTES, SIZE, MIN_VALUE, MAX_VALUE

---
+++ Part01/Java100_variable_003.java
@@ -0,0 +1,11 @@
+public class Java100_variable_003 {
+	public static void main(String[] args) {
+		System.out.println("Byte:\t" + Byte.BYTES + " byte\t| " + Byte.SIZE + " bit\t(" + Byte.MIN_VALUE + " ~ " + Byte.MAX_VALUE + ")");
+		System.out.println("Short:\t" + Short.BYTES + " byte\t| " + Short.SIZE + " bit\t(" + Short.MIN_VALUE + " ~ " + Short.MAX_VALUE + ")");
+		System.out.println("Int:\t" + Integer.BYTES + " byte\t| " + Integer.SIZE + " bit\t(" + Integer.MIN_VALUE + " ~ " + Integer.MAX_VALUE + ")");
+		System.out.println("Long:\t" + Long.BYTES + " byte\t| " + Long.SIZE + " bit\t(" + Long.MIN_VALUE + " ~ " + Long.MAX_VALUE + ")");
+		System.out.println("Char:\t" + Character.BYTES + "byte\t| " + Character.SIZE + " bit\t(" + Character.MIN_VALUE + " ~ " + Character.MAX_VALUE + ")");
+		System.out.println("Char:\t" + Character.BYTES + "byte\t| " + Character.SIZE + " bit\t(" + (int)Character.MIN_VALUE + " ~ " + (int)Character.MAX_VALUE + ")");
+		
+	}
+}
---
+++ Part01/Java100_variable_DataType2.java
@@ -0,0 +1,5 @@
+public class Java100_variable_DateType2{
+	public static void main(String[] args) {
+		
+	}
+}
