cebf2aad47ddf9a246e53c35486598f44dba0e77
Yulia <oing922@gmail.com>
2022-03-15 18:10:07 +0900

    array basic

---
+++ Part02/Java100_array_Basic001.java
@@ -0,0 +1,17 @@
+public class Java100_array_Basic001 {
+	public static void main(String[] args) {
+		
+		int[] sales_table = new int[100];
+		System.out.println(sales_table);
+		
+		double sales_table2[] = new double[10];
+		System.out.println(sales_table2);
+		
+		String[] sales_table3 = new String[5];
+		System.out.println(sales_table3);
+		
+		sales_table[9] = 10;
+		System.out.println(sales_table[9]);
+		
+	}
+}
