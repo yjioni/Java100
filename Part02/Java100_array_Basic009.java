01d3f088a1d2696db16c5903cb0508fc53e8b311
Yulia <oing922@gmail.com>
2022-03-17 08:33:11 +0900

    System.arraycopy(복사할배열, 복사 시작위치, 붙여넣기배열, 붙여넣기 위치, 길이); 연습2

--- Part02/Java100_array_Basic009.java
+++ Part02/Java100_array_Basic009.java
@@ -2,16 +2,10 @@ import java.util.Arrays;
 
 public class Java100_array_Basic009{
 	public static void main(String[] args){
-		
-		int[] ar = {1, 2, 3, 4, 5};
-		System.out.println(Arrays.toString(ar));
+		int[] ar1 = {1, 2, 3, 4, 5};
 		int[] ar2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
+		System.arraycopy(ar1, 1, ar2, 5, 4);
 		System.out.println(Arrays.toString(ar2));
-		
-		int[] ar_copy;
-		
-		// System.arraycopy(복사대상, 복사대상-시작인덱스, 붙여넣기 대상, 붙여넣기-시작인덱스, 배열길이);
-		System.arraycopy(ar, 1, ar2, 4, 3);
-		System.out.println(Arrays.toString(ar2));		
 	}
 }
