ca302d5d298f868f3b76d615b865c2ea062fc0ef
Yulia <oing922@gmail.com>
2022-03-22 17:32:03 +0900

    call by value + using common method

--- Part03/Java100_method_MethodCall1.java
+++ Part03/Java100_method_MethodCall1.java
@@ -4,9 +4,20 @@ public class Java100_method_MethodCall1{
 		System.out.println(a);
 	}
 	
+	public void sum2(int a2){
+		a2 += 100;
+		System.out.println(a2);
+	}
+	
 	public static void main(String[] args){
+		
 		int a = 400;
-		sum(a);
 		System.out.println(a);
+		
+		sum(a);
+				
+		Java100_method_MethodCall1 jmm = new Java100_method_MethodCall1();
+		jmm.sum2(a);
+		
 	}
 }
