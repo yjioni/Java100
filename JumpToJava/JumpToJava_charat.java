db6247396c8174a5c0087bf4dadc0a3d04bc105b
Yulia <oing922@gmail.com>
2022-03-28 13:28:07 +0900

    JumpToJava-basic

---
+++ JumpToJava/JumpToJava_charat.java
@@ -0,0 +1,7 @@
+public class JumpToJava_charat{
+	public static void main(String[] args){
+		String a = "Hello Java";
+		
+		System.out.println(a.charAt(0));
+	}
+}
---
+++ JumpToJava/JumpToJava_contains.java
@@ -0,0 +1,7 @@
+public class JumpToJava_contains{
+	public static void main(String[] args){
+		String a = "Hello Java";
+		
+		System.out.println(a.contains("Java"));
+	}
+}
