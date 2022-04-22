23349d5a5e59ff56818c5b97a1e7603438b35e9d
Yulia Yoon <88180839+yjioni@users.noreply.github.com>
2022-04-22 17:56:08 +0900

    Add files via upload
    로또 번호 생성기 -수정1 method로 나누기

--- MyLotto.java
+++ MyLotto.java
@@ -4,59 +4,98 @@ import java.util.Scanner;
 import java.io.*;
 import java.util.Collections;
 
-public class MyLotto1{
-	public static void main(String[] args){
-		// [1]변수 생성: 사용자 입력
+public class MyLotto{
+	
+	// 사용자 입력 함수
+	static ArrayList<Integer> inputNumber(){
+		// [1]변수 생성
 		Scanner scanner = new Scanner(System.in);
+		
 		System.out.print("1~50 사이의 번호를 입력하세요: ");
 		String user = scanner.nextLine();
-		
-		// [2]쉼표로 분리 & 형변환
+	
+		// [2]쉼표로 분리 & 형변환 & 유효값 확인 
 		String[] str = user.split("\\s*,\\s*");
-		// ArrayList 선언
 		ArrayList<Integer> userAL = new ArrayList<Integer>();
-		//형변환 하면서 ArrayList에 넣기
-		for (int i=0;i<str.length;i++){ userAL.add(Integer.parseInt(str[i])); }
+		for (int i=0;i<str.length;i++){ 
+			int temp = Integer.parseInt(str[i]);
+			if (temp>0 && temp<51){
+				userAL.add(temp); 
+			}
+			else { System.out.print("입력하신 숫자" + temp + "는 범위 초과로 삭제합니다.");}
+		}
 		
-		// [3]중복 제거
+		return userAL;
+	}
+	
+	// 중복 제거 함수
+	static ArrayList<Integer> deleteSameNumber(ArrayList<Integer> userAL){		
+		
 		ArrayList<Integer> lottoAL = new ArrayList<Integer>();
 		
 		for (int i=0;i<userAL.size();i++){
-			// System.out.println(userAL.get(i));
 			if (!lottoAL.contains( userAL.get(i) )){ lottoAL.add( userAL.get(i) ); }
 		}
 		
-		// [4]요소 값이 6 이하이면, 랜덤숫자 추가
+		return lottoAL;
+	}
+	
+	// 자동 번호 함수
+	static ArrayList<Integer> autoNumber(ArrayList<Integer> lottoAL){
+		// 요소가 6개 이하면, 추가
 		while (lottoAL.size()<6){
 			lottoAL.add((int)(Math.random() * 50 +1 ));
 		}
-		// 확인 출력
-		// System.out.println("---------");
-		// for (int i=0;i<lottoAL.size();i++){ System.out.print(lottoAL.get(i) +" "); }
-		// System.out.println("---------");
-		System.out.println();
-		
-		// [5]오름차순 정렬 
+		return lottoAL;
+	}
+
+	// 출력 저장 함수
+	static void printSave(ArrayList<Integer> lottoAL, String addr){
+		// 오름차순 정렬 
 		Collections.sort(lottoAL);
-		
+	
 		// [6].txt로 저장하기 
-		String fileNm = "C:\\Users\\oing9\\Desktop\\date_file.txt";
 		try{
-			File file = new File(fileNm);
+			File file = new File(addr); // 파라미터 addr
 			BufferedWriter fileWrite = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "utf-8"));
 			
 			for (int i=0; i<lottoAL.size(); i++){
 				String text = Integer.toString(lottoAL.get(i));
 				text += " ";
 				fileWrite.write(text);
-				// 확인 출력
-				// System.out.print(lottoAL.get(i) + " ");
 			}
 			
 			fileWrite.flush(); 
 			fileWrite.close();
 			} catch (Exception e){
 				e.printStackTrace(); 
-			}
 		}
 	}
+	
+	public static void main(String[] args){
+		// 사용자 입력 ArrayList
+		ArrayList<Integer> userInput = new ArrayList<Integer>();
+		userInput = inputNumber();
+		
+		System.out.println("사용자 입력 번호: " + userInput);
+		
+		// 자동 번호 추가 입력 ArrayList
+		ArrayList<Integer> compNumber = new ArrayList<Integer>();
+		
+		while (compNumber.size() < 6){
+			compNumber = autoNumber(userInput); 
+			compNumber = deleteSameNumber(compNumber); // 중복 제거
+		}
+		
+		System.out.println("최종 로또 번호: " + compNumber);
+		
+		// 출력 및 저장
+		
+		String addr = "C:\\Users\\admin\\Documents\\Java\\theBeeSoft\\MyLotto.txt";
+		printSave(compNumber, addr);
+		System.out.println("저장 위치: " + addr);
+		System.out.println("저장한 로또 번호: " + compNumber);
+		
+		
+	}
+}
