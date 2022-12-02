f968ef5202bd251680b6a901052030e89ad6988a
jwyoon <oing922@gmail.com>
2022-12-02 22:30:59 +0900

    Property ex1

--- JavaBasic_2/src/PropertiesEx1.java
+++ JavaBasic_2/src/PropertiesEx1.java
@@ -8,31 +8,33 @@ class PropertiesEx1 {
 		
 		prop.setProperty("timeout", "30");
 		prop.setProperty("language", "KR");
-		prop.setProperty("size", "10");
-		prop.setProperty("capacity", "10");
-		
+	//	prop.setProperty("size", 100);   // String type 
+		prop.setProperty("capacity", "100");
+		prop.setProperty("size", "100");
 		
 		Enumeration e = prop.propertyNames();
 		
 		while (e.hasMoreElements()) {
 			
-			String element = (String)e.nextElement();
+			String element = (String) e.nextElement();
 			System.out.println(element + "=" + prop.getProperty(element));
 			
 		}
 		
 		System.out.println();
-		prop.setProperty("size", "20");
+		prop.setProperty("size", "300");
 		
-		System.out.println("name=" + prop.getProperty("name", "hoseong"));
+		System.out.println("name=" + prop.getProperty("name", "Jonh"));
 		System.out.println("size=" + prop.getProperty("size"));
-		System.out.println("capacity=" + prop.getProperty("capacity", "200"));
-		System.out.println("loadfactor=" + prop.getProperty("loadfactor", "0.75"));
+		System.out.println("capacity=" + prop.getProperty("capacity"));
+		System.out.println("number=" + prop.getProperty("number"));  // 없는 key 값일 경우, Null
 		
 		System.out.println();
-		System.out.println(prop);
+		System.out.println(prop);  //{size=300, language=KR, timeout=30, capacity=100}
 		System.out.println();
+		
 		prop.list(System.out);
+				
 	}
 	
 }
