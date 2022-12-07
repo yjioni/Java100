cfe85c73bc2f379615b6ec5e3e4553212e2d8388
jwyoon@cnspartner <jwyoon@thebeesoft.com>
2022-12-08 08:56:20 +0900

    BJTest 사칙연산

---
+++ JavaBasic_2/src/BJTest_1001.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class BJTest_1001 {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, minus;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		minus = a-b;
+		System.out.println(sum);		
+	
+	}
+}
