564267ee272d07433acebd7a99c14185cbd73896
jwyoon@cnspartner <jwyoon@thebeesoft.com>
2022-12-08 09:00:56 +0900

    BJTest 사칙연산

---
+++ JavaBasic_2/src/BJTest_1008.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BJTest_1008{
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		Double a, b, div;
+		
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		
+		div = a/b;
+			
+		System.out.println(div);
+	
+	}
+}
