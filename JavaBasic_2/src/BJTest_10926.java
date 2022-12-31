a9156867565d70442ae33dd926524431730446bc
jwyoon <oing922@gmail.com>
2022-12-31 21:49:13 +0900

    12/31 BJTest 10926 입력되는 ID값 뒤에 "??!" 붙여 출력하기.

---
+++ JavaBasic_2/src/BJTest_10926.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BJTest_10926{
+   public static void main (String[] args) {
+
+	   Scanner sc = new Scanner(System.in);
+
+       String inputId = sc.nextLine();
+       System.out.println(inputId + "??!");
+   }
+}
